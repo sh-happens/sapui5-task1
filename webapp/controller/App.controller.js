987a51ae2c9b5897234ab5ee7739d9c86e3c14db
Yerbol Kosbayev <yerbol_kosbayev@list.ru>
2025-11-04 19:05:45 +0500

    adds modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], (Controller) => {
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], (Controller, MessageToast) => {
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
       onShowHello() {
-         alert("Hello World");
+         MessageToast.show("Hello World");
       }
    });
 });
