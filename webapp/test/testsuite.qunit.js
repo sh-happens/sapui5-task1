64d967a15c8572ab2d8c0cb795a007a4a572f450
Yerbol Kosbayev <yerbol_kosbayev@list.ru>
2025-11-08 18:07:36 +0500

    adds integration tests

--- webapp/test/testsuite.qunit.js
+++ webapp/test/testsuite.qunit.js
@@ -19,6 +19,9 @@ sap.ui.define(() => {
         tests: {
             "unit/unitTests": {
                 title: "UI5 Walkthrough - Unit Tests"
+            },
+            "integration/opaTests": {
+                title: "UI5 Walkthrough - Integration Tests"
             }
         }
     };
